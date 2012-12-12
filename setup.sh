b227036dba9b56ed846d4819b0efebd1f77256b5
David Elliott <david@hadto.net>
2012-12-11 19:00:30 -0800

    run as sudo

--- setup.sh
+++ setup.sh
@@ -1,28 +1,28 @@
 #! /bin/sh
 
-sudo sh -c 'echo "deb-src http://archive.raspbian.org/raspbian wheezy main contrib non-free rpi" >> /etc/apt/sources.list'
+sh -c 'echo "deb-src http://archive.raspbian.org/raspbian wheezy main contrib non-free rpi" >> /etc/apt/sources.list'
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get upgrade
+apt-get upgrade
 
 wget https://puredata.info/downloads/pd-extended-0-43-3-on-raspberry-pi-raspbian-wheezy-armhf/releases/1.0/Pd-0.43.3-extended-20121004.deb
 
-sudo dpkg -i Pd-0.43.3-extended-20121004.deb
+dpkg -i Pd-0.43.3-extended-20121004.deb
 
-sudo apt-get -f install
+apt-get -f install
 
-sudo chmod 4755 /usr/bin/pd-extended 
+chmod 4755 /usr/bin/pd-extended 
 
-sudo apt-get install python-pip vim emacs byobu ipython
+apt-get install python-pip vim emacs byobu ipython
 
-sudo cp rpi_setup/pdsend /usr/local/bin
+cp rpi_setup/pdsend /usr/local/bin
 
-sudo cp rpi_setup/pdreceive /usr/local/bin
+cp rpi_setup/pdreceive /usr/local/bin
 
-echo "alias pd='/usr/lib/pd-extended/bin/pd'" >> /.bashrc
+"alias pd='/usr/lib/pd-extended/bin/pd'" >> /.bashrc
 
-sudo modprobe snd-bcm2835
+modprobe snd-bcm2835
 
-sudo amixer cset numid=3 1
+amixer cset numid=3 1
 
